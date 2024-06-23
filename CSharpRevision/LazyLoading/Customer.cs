e9a7da95b6b6dd35bff8821f380b5fff9658d6ab
sh.saha <shourovsaha94@gmail.com>
2024-06-23 13:54:02 +0600

    Lazy loading implementation

---
+++ CSharpRevision/LazyLoading/Customer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoading
+{
+    public class Customer
+    {
+        private Lazy<List<Order>> _orders = null;
+        public List<Order> Orders
+        {
+            get
+            {
+                return _orders.Value;
+            }
+        }
+
+        private string _customerName;
+        public string CustomerName
+        {
+            get
+            {
+                return _customerName;
+            }
+            set
+            {
+                _customerName = value;
+            }
+        }
+
+        public Customer()
+        {
+            _customerName = "S Saha";
+            _orders = new Lazy<List<Order>>(loadOrders());
+        }
+
+        private List<Order> loadOrders()
+        {
+            List<Order> orders = new List<Order>();
+            Order order = new Order
+            {
+                OrderId = "odr10001",
+            };
+            orders.Add(order);
+
+            order = new Order
+            {
+                OrderId = "odr10002",
+            };
+            orders.Add(order);
+            return orders;
+        }
+    }
+}
---
+++ CSharpRevision/LazyLoading/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LazyLoading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Customer customer = new Customer();
+
+            Console.WriteLine(customer.CustomerName);
+
+            foreach (var item in customer.Orders)
+            {
+                Console.WriteLine(item.OrderId);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
